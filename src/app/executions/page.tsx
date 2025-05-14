46cf628a73d15158e57f793fe9976e8a37740530
talisk21 <talisk@i.ua>
2025-05-14 17:22:03 +0300

    configure project, add some components

--- src/app/executions/page.tsx
+++ src/app/executions/page.tsx
@@ -1,67 +1,86 @@
 'use client';
 
-import { useEffect, useState, useTransition } from 'react';
+import {useCallback, useEffect, useState, useTransition} from 'react';
 import { useRouter } from 'next/navigation';
 import {apiMain} from "@/lib/axiosInstance";
-
+import {ExecutionType} from "@/types/execution";
+import Loader from '@/components/Loader';
+import Button from "@/components/Button";
+import './styles.scss';
+import Card from "@/components/Card";
+import {formatDateTimeToStringWithDot} from "@/utils/date";
 
 
 export default function ExecutionsPage() {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const [executions, setExecutions] = useState<any[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const [executions, setExecutions] = useState<ExecutionType[]>([]);
     const [isPending, startTransition] = useTransition();
     const router = useRouter();
 
-    const loadExecutions = async () => {
-        console.log('res - api:', apiMain);
-        const res = await apiMain.get('/executions');
-        console.log('res - executions:', res);
-        const data = await res.data;
-        setExecutions(data);
-    };
+
 
     useEffect(() => {
-        console.log('Executions page loaded');
+        const loadExecutions = async () => {
+            setLoading(true);
+            try {
+                const res = await apiMain.get('/executions');
+                console.log('res - executions:', res);
+                const data = await res.data;
+                setExecutions(data);
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
         loadExecutions();
     }, []);
 
-    const createExecution = async () => {
-        const res = await await apiMain.get('/executions', {
+    const createExecution = useCallback(async () => {
+        const res = await apiMain.get('/executions', {
             method: 'POST',
         });
         const data = await res.data;
         router.push(`/executions/${data.id}`);
-    };
+    }, [router]);
 
     return (
-        <main className="p-6 bg-white text-black">
-            <div className="flex justify-between items-center mb-6">
-                <h1 className="text-2xl font-bold">Executions</h1>
-                <button
-                    onClick={() => startTransition(createExecution)}
-                    className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 disabled:opacity-50"
-                    disabled={isPending}
-                >
-                    {isPending ? 'Создание...' : '➕ Новый запуск'}
-                </button>
-            </div>
-
-            <ul className="space-y-3">
-                {executions.map((e) => (
-                    <li
-                        key={e.id}
-                        className="p-4 bg-white border rounded text-sm shadow"
+        <main className="main">
+            {loading && <Loader />}
+            <div className="executions-page page-container">
+                <div className="executions-page__header">
+                    <h1 className="">Executions</h1>
+                    <Button
+                        onClick={() => startTransition(createExecution)}
+                        disabled={isPending}
                     >
-                        <a href={`/executions/${e.id}`} className="text-blue-700 underline">
-                            <strong>ID:</strong> {e.id}
-                        </a>
-                        <br />
-                        <strong>Status:</strong> {e.status}
-                        <br />
-                        <strong>Started:</strong> {e.startedAt}
-                    </li>
-                ))}
-            </ul>
+                        {isPending ? 'Создание...' : '➕ Новый запуск'}
+                    </Button>
+                </div>
+
+
+
+                <ul className="executions-page__execution-list">
+                    {executions.map((e) => (
+                        <li
+                            key={e.id}
+                            className=""
+                        >
+                            <Card>
+                                <a href={`/executions/${e.id}`} >
+                                <strong>ID:</strong> {e.id}
+                                </a>
+                                <br />
+                                <strong>Status:</strong> {e.status}
+                                <br />
+                                <strong>Started:</strong> {formatDateTimeToStringWithDot(e.startedAt)}
+                            </Card>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </main>
     );
 }
